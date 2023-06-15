6fa5f2fe4d11216b989f3e5b0dbc298fe91501b6
marshmallow <marshycity@gmail.com>
2023-06-15 18:34:08 +1000

    add framework powersaving stuff

--- configuration.nix
+++ configuration.nix
@@ -104,6 +104,33 @@
 
   services.fprintd.enable = true;
 
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      # https://wiki.archlinux.org/title/Framework_Laptop_13#12th_gen_Turbo-Boost_on_battery_with_tlp
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+      # https://community.frame.work/t/guide-linux-battery-life-tuning/6665
+      PCIE_ASPM_ON_BAT = "powersupersave";
+    };
+  };
+  powerManagement.powertop.enable = true;
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+  };
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
