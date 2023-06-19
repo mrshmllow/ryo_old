b81fc0eaf6b8208147cffb2712997b4694c53e33
marshmallow <marshycity@gmail.com>
2023-06-20 09:29:58 +1000

    remove reduntant autoLogin.enable

--- common.nix
+++ common.nix
@@ -80,8 +80,6 @@
   programs.fish.enable = true;
   environment.pathsToLink = ["/share/bash-completion"];
 
-  services.xserver.displayManager.autoLogin.enable = true;
-
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
