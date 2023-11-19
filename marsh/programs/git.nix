81f99f4873b9b8eaa44765c44f109507c13119a1
marshmallow <marshycity@gmail.com>
2023-11-19 14:13:21 +1100

    stop signing everything

--- marsh/programs/git.nix
+++ marsh/programs/git.nix
@@ -6,10 +6,10 @@
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
-    signing = {
-      key = "CE4CECD4861112D38ED3393A767B8880F5AAEB9C";
-      signByDefault = true;
-    };
+    # signing = {
+    #   key = "CE4CECD4861112D38ED3393A767B8880F5AAEB9C";
+    #   signByDefault = true;
+    # };
     userEmail = "marshycity@gmail.com";
     userName = "marshmallow";
     extraConfig = {
