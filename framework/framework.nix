6fb2c3e930187e165cae750746c0c792cd9a1ed3
marshmallow <marshycity@gmail.com>
2023-12-27 17:23:43 +1100

    improve TLP Config following offical guides

--- framework/framework.nix
+++ framework/framework.nix
@@ -13,11 +13,58 @@
   services.tlp = {
     enable = true;
     settings = {
-      # https://wiki.archlinux.org/title/Framework_Laptop_13#12th_gen_Turbo-Boost_on_battery_with_tlp
-      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-      # https://community.frame.work/t/guide-linux-battery-life-tuning/6665
+      # https://knowledgebase.frame.work/en_us/optimizing-ubuntu-battery-life-Sye_48Lg3
+      INTEL_GPU_MIN_FREQ_ON_AC = 100;
+      INTEL_GPU_MIN_FREQ_ON_BAT = 100;
+
+      INTEL_GPU_MAX_FREQ_ON_AC = 1500;
+      INTEL_GPU_MAX_FREQ_ON_BAT = 800;
+
+      INTEL_GPU_BOOST_FREQ_ON_AC = 1500;
+      INTEL_GPU_BOOST_FREQ_ON_BAT = 1000;
+
+      WIFI_PWR_ON_AC = false;
+      WIFI_PWR_ON_BAT = false;
+
+      PCIE_ASPM_ON_AC = "default";
       PCIE_ASPM_ON_BAT = "powersupersave";
+
+      RUNTIME_PM_ON_AC = false;
+      RUNTIME_PM_ON_BAT = true;
+
+      GPU_SCALING_GOVERNOR_ON_AC = "performance";
+      GPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 30;
+
+      CPU_BOOST_ON_AC = true;
+      CPU_BOOST_ON_BAT = true;
+
+      CPU_HWP_DYN_BOOST_ON_AC = true;
+      CPU_HWP_DYN_BOOST_ON_BAT = false;
+
+      SCHED_POWERSAVE_ON_AC = false;
+      SCHED_POWERSAVE_ON_BAT = true;
+
+      NMI_WATCHDOG = false;
+
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+
+      USB_AUTOSUSPEND = true;
+      USB_EXCLUDE_AUDIO = true;
+      USB_EXCLUDE_BTUSB = true;
+      USB_EXCLUDE_PHONE = true;
+      USB_EXCLUDE_PRINTER = true;
+      USB_EXCLUDE_WWAN = true;
+
+      USB_AUTOSUSPEND_DISABLE_ON_SHUTDOWN = false;
     };
   };
 
