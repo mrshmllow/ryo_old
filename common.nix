85e2e97bc3f34d7e9cd76c393f596a1b2b23e279
marshmallow <marshycity@gmail.com>
2023-12-29 23:23:19 +1100

    add nix-community subtituter

--- common.nix
+++ common.nix
@@ -8,6 +8,13 @@
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
     auto-optimise-store = true;
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   nix.gc = {
