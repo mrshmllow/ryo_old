885c5964022e7c0e06f0b0c288490ac68a7177df
marshmallow <marshycity@gmail.com>
2024-01-03 13:25:41 +1100

    add osu to maple

--- maple/configuration.nix
+++ maple/configuration.nix
@@ -9,5 +9,9 @@
   boot.initrd.luks.devices."luks-238f587f-cba0-43d1-ac83-89f480c8fa08".device = "/dev/disk/by-uuid/238f587f-cba0-43d1-ac83-89f480c8fa08";
   networking.hostName = "maple";
 
+  users.users.marsh.packages = with pkgs; [
+    osu-lazer-bin
+  ];
+
   system.stateVersion = "23.11";
 }
