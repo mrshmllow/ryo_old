de2af06f5426936701f610e87e8d681cd48cbe91
marshmallow <marshycity@gmail.com>
2024-01-20 00:05:58 +1100

    duup driSupport32bit

--- hosts/maple/hardware-configuration.nix
+++ hosts/maple/hardware-configuration.nix
@@ -44,6 +44,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  hardware.opengl.driSupport32Bit = true;
 }
