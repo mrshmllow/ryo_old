123c63c5fc43f7f400f37a75eaf3f6a1fd2766f5
marshmallow <marshycity@gmail.com>
2024-02-03 11:14:19 +1100

    strip unneeded stuff for blender

--- hosts/maple/blender.nix
+++ hosts/maple/blender.nix
@@ -11,12 +11,6 @@
 
   hardware.opengl.extraPackages = with pkgs; [
     rocmPackages.clr.icd
-    amdvlk
-  ];
-
-  # For 32 bit applications
-  hardware.opengl.extraPackages32 = with pkgs; [
-    driversi686Linux.amdvlk
   ];
 
   hardware.opengl.driSupport32Bit = true;
