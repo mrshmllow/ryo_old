abbdb5efbc2348a4e66bd27bbfaaed4544410a5b
marshmallow <marshycity@gmail.com>
2024-02-04 21:12:08 +1100

    move a lot of apps out of flatpak

--- desktop/apps.nix
+++ desktop/apps.nix
@@ -48,6 +48,11 @@
     vieb
     prismlauncher
     mangohud
+    gimp
+    transmission
+    element-desktop
+    jellyfin-media-player
+    vesktop
   ];
 
   services.flatpak.enable = true;
@@ -56,18 +61,7 @@
     "launchermoe" = "https://gol.launcher.moe/gol.launcher.moe.flatpakrepo";
   };
   services.flatpak.packages = [
-    "flathub:app/org.gnome.Builder/x86_64/stable"
-    "flathub:app/org.gimp.GIMP/x86_64/stable"
-    "flathub:app/org.gnome.Boxes/x86_64/stable"
-    "flathub:app/com.transmissionbt.Transmission/x86_64/stable"
-    "flathub:app/im.riot.Riot/x86_64/stable"
-    "flathub:app/rest.insomnia.Insomnia/x86_64/stable"
     "flathub:app/md.obsidian.Obsidian/x86_64/stable"
-    "flathub:app/com.github.iwalton3.jellyfin-media-player/x86_64/stable"
-    "flathub:app/com.heroicgameslauncher.hgl/x86_64/stable"
-    "flathub:runtime/org.gnome.Platform/x86_64/44"
-    "flathub:app/io.github.jeffshee.Hidamari/x86_64/stable"
-    "flathub:app/com.spotify.Client/x86_64/stable"
     "flathub:app/dev.vencord.Vesktop/x86_64/stable"
 
     "launchermoe:app/moe.launcher.anime-games-launcher/x86_64/stable"
